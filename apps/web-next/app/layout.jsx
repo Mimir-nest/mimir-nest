56ff7db94602ec545507a213d366edf2929d0655
mugenkyou <sachinskyte@gmail.com>
2026-08-29 02:20:58 +0530

    favicon update

--- apps/web-next/app/layout.jsx
+++ apps/web-next/app/layout.jsx
@@ -43,6 +43,18 @@ export const metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "512x512" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", type: "image/png", sizes: "180x180" },
+    ],
+  },
+  manifest: "/site.webmanifest",
   openGraph: {
     type: "website",
     locale: "en_US",
